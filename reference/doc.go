14c63aa81e555ec6484403598271bc7aca4f1d64
nolash <dev@holbrook.no>
2020-04-03 08:21:05 +0200

    Add copyright headers

--- reference/doc.go
+++ reference/doc.go
@@ -1,7 +1,7 @@
 // Copyright 2020 The Swarm Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-//
+
 // Package bmt is a simple nonconcurrent reference implementation for hashsize segment based
 // Binary Merkle tree hash on arbitrary but fixed maximum chunksize
 //
