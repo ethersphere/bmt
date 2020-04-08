e6b105c338ce6c71bf833330e77a2ac274e4024e
lash <nolash@users.noreply.github.com>
2020-04-08 10:02:24 +0200

    Add interface and correctness reference (#1)

--- errors.go
+++ errors.go
@@ -2,5 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package bmt defines the interface for the Binary Merkle Tree hash.
 package bmt
+
+import (
+	"errors"
+)
+
+var ErrOverflow = errors.New("BMT hash capacity exceeded")
